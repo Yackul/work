3c4dd6af2fab8a5a31104ad827acee3dfef78780
Travis <travis.lamb@bellevuecollege.edu>
2020-11-19 12:40:03 -0800

    notification system

--- logintest/src/index.js
+++ logintest/src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LogIn from "./LogInPage";
 import RegIn from "./Registration";
+import HPage from "./Homepage";
+
  const rootElement = document.getElementById("root");
  ReactDOM.render(
    <BrowserRouter>
     <Switch>
      <Route exact path="/" component={LogIn} />
      <Route path="/Register" component={RegIn} />
+     <Route path="/Home" component={HPage} />
    </Switch>
    </BrowserRouter>,
    rootElement
