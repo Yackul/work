b1e97e30854356584fca4545285d8db08ed62aa2
christinavii85 <64942320+christinavii85@users.noreply.github.com>
2020-11-19 20:31:14 -0800

    Add files via upload

---
+++ NotificationSystem_Scratch/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import './App.css';
+import {useNotification} from "./Notification/NotificationProvider";
+
+function App() {
+  const [inputVal, setInputVal] = useState("");
+  const dispatch = useNotification();
+
+  const handleNewNotification = () => {
+    dispatch({
+      type: "ERROR",
+      message: inputVal,
+      title: "Successful Request"
+    })
+  }
+
+  return (
+    <div className="App">
+      <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
+      <button onClick={handleNewNotification}>Add Notification</button>
+    </div>
+  );
+}
+
+export default App;
