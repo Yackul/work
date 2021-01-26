d8c930ffb97b8dd04cbb26156a663952f795a783
Yackul <lambatravis@gmail.com>
2021-01-25 16:27:44 -0800

    local commit of various changes

--- React/src/Homepage.js
+++ React/src/Homepage.js
@@ -3,6 +3,9 @@ import './index.css';
 import ReviewCreator from './ReviewCreator'
 import logo from './GitGoing.jpeg';
 import PropTypes from 'prop-types';
+import { Auth } from 'aws-amplify'
+
+
 
 class Homepage extends React.Component {
 
@@ -10,7 +13,17 @@ class Homepage extends React.Component {
         super(props)
         this.state = {
             isOpen: false,
+            authState: 'loading'
           };
+    }
+
+    componentDidMount = async () => {
+        try {
+          await Auth.currentAuthenticatedUser()
+          this.setState({ authState: 1 })
+        } catch (err) {
+          this.setState({ authState: 'unauthorized' })
+        }
     }
 
     toggleModal = () => {
@@ -19,26 +32,36 @@ class Homepage extends React.Component {
 
     render() {
 
-        return (    
-            <div> 
-                <div className="pill-nav">
-                    <img src={logo} alt="avatar2" className="avatar2" />
-                    <a href="/Home">Home</a>
-                    <a href="/Me">My Profile</a>
-                    <a href="/MyProjects">My Projects</a>
+        switch(this.state.authState) {
+            case('loading'):
+                return <h1>Loading</h1>
+            case(1):
+                return (    
+                    <div> 
+                    <div className="pill-nav">
+                        <img src={logo} alt="avatar2" className="avatar2" />
+                        <a href="/Home">Home</a>
+                        <a href="/Me">My Profile</a>
+                        <a href="/MyProjects">My Projects</a>
+                    </div>
+                    <br></br>
+                    <h1>Welcome to Git Going!</h1>
+                    <p> Provide us a repo path and file name through a JSON file with the create button below, and we'll give you a diff!</p>
+                     <button onClick={this.toggleModal}>
+                        Create Code Review
+                    </button>
+                    <br></br>
+                    <ReviewCreator show={this.state.isOpen}
+                           onClose={this.toggleModal}>
+                    </ReviewCreator>
                 </div>
-                <br></br>
-                <h1>Welcome to Git Going!</h1>
-                <p> Provide us a repo path and file name through a JSON file with the create button below, and we'll give you a diff!</p>
-                 <button onClick={this.toggleModal}>
-                    Create Code Review
-                </button>
-                <br></br>
-                <ReviewCreator show={this.state.isOpen}
-                       onClose={this.toggleModal}>
-                </ReviewCreator>
-            </div>
-          );
+              );
+            case('unauthorized'):
+                return window.location = "/"
+            default: 
+                return null  
+        }
+
     }
 }
 
--- React/src/Profile.js
+++ React/src/Profile.js
@@ -1,39 +1,66 @@
 import React from 'react';
 import './index.css';
 import logo from './GitGoing.jpeg';
+import {Auth} from 'aws-amplify';
 
 class ProfilePage extends React.Component {
     
   constructor(props) {
     super(props);
     this.state = {
+      authState: 'loading'
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount = async () => {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.setState({ authState: 1 })
+    } catch (err) {
+      this.setState({ authState: 'unauthorized' })
+    }
+}
+
+  signOut = async () => {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
+
   handleSubmit(evt) {
     evt.preventDefault();
-    window.location = "/"
   }  
   
   render() {
     
-    return (    
-        
-      <div> 
-        <div className="pill-nav">
-        <img src={logo} alt="avatar2" className="avatar2" />
-            <a href="/Home">Home</a>
-            <a href="/Me">My Profile</a>
-            <a href="/MyProjects">My Projects</a>
-          </div>
-        <h2>Im a profile!</h2>
-        <input type ="submit" className="submit" onClick={this.handleSubmit} value = "Sign Out"/>
-           </div>
-          
-        
-      );
+    switch(this.state.authState) {
+      case('loading'):
+          return <h1>Loading</h1>
+      case(1):
+          return (    
+            <div> 
+            <div className="pill-nav">
+            <img src={logo} alt="avatar2" className="avatar2" />
+                <a href="/Home">Home</a>
+                <a href="/Me">My Profile</a>
+                <a href="/MyProjects">My Projects</a>
+              </div>
+            <h2>Im a profile!</h2>
+            <input type ="submit" className="submit" onClick={this.signOut} value = "Sign Out"/>
+               </div>
+          );
+      case('unauthorized'):
+          return window.location = "/"
+      default: 
+          return null  
+  }
+
+
+
     }
   }
 
