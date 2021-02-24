c0ce66812f3f252fde1b6377eb5cfa5f6f28f80c
christinavii85 <64942320+christinavii85@users.noreply.github.com>
2021-02-23 22:58:52 -0800

    Add files via upload

--- React/src/Profile.js
+++ React/src/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import NavBar from './NavBar'
+import logo from './GitGoing.jpeg';
 import { Auth } from 'aws-amplify';
 
 class ProfilePage extends React.Component {
@@ -8,32 +8,33 @@ class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      authState: 'loading',
-      Uname: ''
+      authState: 'loading'
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount = async () => {
-    console.log('componentDidMount calledHP')
     try {
       await Auth.currentAuthenticatedUser()
-      const tokens = await Auth.currentSession();
-      const userName = tokens.getIdToken().payload['cognito:username'];
-      var userNameHold = userName.charAt(0).toUpperCase() + userName.slice(1);
-      this.setState({ authState: 1,
-        Uname: userNameHold })
+      this.setState({ authState: 1 })
     } catch (err) {
       this.setState({ authState: 'unauthorized' })
     }
-    //console.log(this.state.authState)
   }
 
+  signOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  }
 
   handleSubmit(evt) {
     evt.preventDefault();
   }
+
   render() {
 
     switch (this.state.authState) {
@@ -42,12 +43,15 @@ class ProfilePage extends React.Component {
       case (1):
         return (
           <div>
-            <NavBar/>
-            <h2>You are: {this.state.Uname}</h2>
-                      
-
+            <div className="pill-nav">
+              <img src={logo} alt="avatar2" className="avatar2" />
+              <a href="/Home">Home</a>
+              <a href="/Me">My Profile</a>
+              <a href="/Projects">My Projects</a>
+            </div>
+            <h2>Im a profile!</h2>
+            <input type="submit" className="submit" onClick={this.signOut} value="Sign Out" />
           </div>
-          
         );
       case ('unauthorized'):
         return window.location = "/"
