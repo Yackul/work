de11a6c1b227633846022b404308159dff3c5561
Michael Bloomquist <michaelrbloomquist@gmail.com>
2021-05-31 09:20:03 -0700

    Added text for selected diff style

--- React/src/RevDis.js
+++ React/src/RevDis.js
@@ -518,11 +518,12 @@ class RevDis extends React.Component {
 
                         {this.state.isReview === 1 &&
                         <div>
-                            <div style={{paddingLeft: 15}}>
+                            <div style={{display: "flex", paddingLeft: 15, alignItems: "center"}}>
                                 <label className="switch">
                                     <input type="checkbox" checked={this.state.isSplit} onChange={this.handleInputChange}/>
                                     <span className="slider"/>
                                 </label>
+                                <p style={{paddingLeft: 10}}>Diff style: {(this.state.isSplit ? "Split diff" : "Unified diff")}</p>
                             </div>
                             <div><input type="submit" className='submit' value="Approve Changes"
                                         onClick={this.approveReview}/>
